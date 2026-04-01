c903e5d28e82128cc59c5576ca16e696fca4f13b
alyssa-beaufils <alyssabeaufils@gmail.com>
2026-04-01 19:31:48 +0200

    file organization

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import './assets/styles/main.scss'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueKonva from 'vue-konva';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueKonva);
+app.mount('#app');
