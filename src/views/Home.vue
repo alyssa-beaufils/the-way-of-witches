d64598322b946d459b0dbed0676e8bc449aad731
alyssa-beaufils <alyssabeaufils@gmail.com>
2026-05-28 12:18:37 +0200

    font import

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
 
 <template>
   <header>
+    <h1>The Way of Witches</h1>
   </header>
 
   <main>
@@ -10,8 +11,15 @@
 </template>
 
 <style lang="scss">
+@import '../assets/styles/variables.scss';
 body {
     background-color: #19001E;
     overflow-y: hidden;
   }
+
+h1{
+    font-family: $font-primary;
+    color: $color-accent;
+}
+
 </style>
