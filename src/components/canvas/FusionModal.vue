8cc92d0f59d3034468e761a2d15a4b34446d21a4
alyssa-beaufils <alyssabeaufils@gmail.com>
2026-08-25 14:14:10 +0200

    swap card feature in fusion modal

--- src/components/canvas/FusionModal.vue
+++ src/components/canvas/FusionModal.vue
@@ -12,19 +12,61 @@
 
     const isCardAReversed = ref(false);
     const isCardBReversed = ref(false);
+    const isSwapped = ref(false);
+    const isDragged = ref(null);
 
-    const toggleCardA = () => { isCardAReversed.value = !isCardAReversed.value; };
-    const toggleCardB = () => { isCardBReversed.value = !isCardBReversed.value; };
+    const orderedCards = computed(() => {
+        const cardAData = {
+            ...props.combo.cardA,
+            originalKey: 'cardA',
+            isReversed: isCardAReversed.value
+        };
+        const cardBData = {
+            ...props.combo.cardB,
+            originalKey: 'cardB',
+            isReversed: isCardBReversed.value
+        };
+        return isSwapped.value ? [cardBData, cardAData] : [cardAData, cardBData];
+    });
+
+    const toggleCard = (originalKey) => {
+        if (originalKey === 'cardA') {
+            isCardAReversed.value = !isCardAReversed.value;
+        } else {
+            isCardBReversed.value = !isCardBReversed.value;
+        }
+    };
 
     const currentActionText = computed(() => {
-        const direction = isCardAReversed.value ? 'reversed' : 'upright';
-        return props.combo.cardA.action[direction];
+        const firstCard = orderedCards.value[0];
+        const direction = firstCard.isReversed ? 'reversed' : 'upright';
+        return firstCard.action[direction];
     });
 
     const currentDomainText = computed(() => {
-        const direction = isCardBReversed.value ? 'reversed' : 'upright';
-        return props.combo.cardB.domain[direction];
+        const secondCard = orderedCards.value[1];
+        const direction = secondCard.isReversed ? 'reversed' : 'upright';
+        return secondCard.domain[direction];
     });
+
+    const onDragStart = (e, index) => {
+        isDragged.value = index;
+        e.dataTransfer.setData('text/plain', index.toString());
+        e.dataTransfer.effectAllowed = 'move';
+    };
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+    };
+
+    const onDrop = (e, targetIndex) => {
+        e.preventDefault();
+        if (isDragged.value !== null && isDragged.value !== targetIndex) {
+            isSwapped.value = !isSwapped.value;
+        }
+        isDragged.value = null;
+    };
 </script>
 
 <template>
@@ -32,35 +74,35 @@
         <div class="modal-container">
             
             <div class="cards-display-zone">
-                <div class="cards-duo-layout">
+                <TransitionGroup name="card-swap" tag="div" class="cards-duo-layout">
                     
-                    <div class="card-wrapper" @click="toggleCardA">
+                    <div
+                        v-for="(card, index) in orderedCards"
+                        :key="card.id || card.originalKey"
+                        class="card-wrapper" 
+                        draggable="true"
+                        @dragstart="onDragStart($event, index)"
+                        @dragover="onDragOver"
+                        @drop="onDrop($event, index)"
+                        @click="toggleCard(card.originalKey)"
+                    >
                         <img 
                             class="card-img"
-                            :src="combo.cardA.image"
-                            :alt="combo.cardA.name"
-                            :class="{'isFlipped' : isCardAReversed}"
+                            :src="card.image"
+                            :alt="card.name"
+                            draggable="false"
+                            :class="{'isFlipped' : card.isReversed}"
                         />
                     </div>
-
-                    <div class="card-wrapper" @click="toggleCardB">
-                        <img 
-                            class="card-img"
-                            :src="combo.cardB.image"
-                            :alt="combo.cardB.name"
-                            :class="{'isFlipped' : isCardBReversed}"
-                        />
-                    </div>
-
-                </div>
-                <p class="click-instruction">Click on a card to flip it and alter the meaning!</p>
+                </TransitionGroup>
+                <p class="click-instruction">Click on a card to flip it and alter the meaning or drag over to swap order!</p>
             </div>
 
             <div class="modal-content">
                 <button class="btn-close-modal" @click="$emit('close')">&#x2715;</button>
                 
                 <h2 class="card-title">
-                    {{ combo.cardA.name }} & {{ combo.cardB.name }}
+                    {{ orderedCards[0].name }} & {{ orderedCards[1].name }}
                 </h2>
 
                 <div class="text-zone">
@@ -139,21 +181,27 @@
     .cards-duo-layout {
         display: flex;
         gap: 20px;
+        position: relative;
     }
 
     .card-wrapper {
         display: flex;
         flex-direction: column;
         align-items: center;
-        cursor: pointer;
+        cursor: grab;
         user-select: none;
+
+        &:active {
+            cursor: grabbing;
+        }
     }
 
     .card-img {
         width: 250px;
         height: auto;
         margin-top: 45px;
-        will-change: transform;
+        user-select: none;
+        -webkit-user-drag: none;
         @include card-tilt-hover;
     }
 
@@ -167,6 +215,7 @@
         font-family: $font-secondary;
         font-size: 14px;
         color: white;
+        user-select: none;
     }
 
     .card-title {
@@ -196,4 +245,7 @@
         text-align: left;
     }
 
+    .card-swap-move {
+        transition: transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1);
+    }
 </style>
